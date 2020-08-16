4e3a4924548bd4e5405a50d0fbc95f386fbab683
seeker1207 <jm3420@naver.com>
2020-08-16 20:33:35 +0900

    테스트 2

--- src/index.js
+++ src/index.js
@@ -32,7 +32,12 @@ class Square extends React.Component {
   
       return (
         <div>
-          
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
           <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
